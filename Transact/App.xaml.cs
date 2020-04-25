3e50c8f9e4da31e63e05edd7588b7d971e1b8bdf
Tyler Cordell <cordellt123@gmail.com>
2020-04-25 18:31:05 -0400

    Setup Prism and App.xaml.

--- Transact/App.xaml.cs
+++ Transact/App.xaml.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using CommonServiceLocator;
+using Prism.Ioc;
+using Prism.Unity;
 
 namespace Transact
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override Window CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
     }
 }
