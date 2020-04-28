551a01dffb8a31e6d7fffd24f1d30b446cc04fef
Tyler Cordell <cordellt123@gmail.com>
2020-04-27 21:01:01 -0400

    Added transaction module, transaction class, and tests.

---
+++ TransactionModule.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TransactionModule.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TransactionModule.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("475dee1c-c8a9-4746-bf75-998c5f35ea14")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TransactionModule/TransactionModule.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+
+namespace TransactionModule
+{
+    public class TransactionModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
