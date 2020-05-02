f7e26870e72f2af09d8690b537209c8ea361feb3
Tyler Cordell <cordellt123@gmail.com>
2020-05-02 12:50:51 -0400

    Improved test coverage for transaction log view model.

---
+++ TransactionModule.Test/ViewModels/TransactionLogViewModelTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransactionModule.ViewModels;
+
+namespace TransactionModule.Test.ViewModels
+{
+    /// <summary>
+    /// Summary description for TransactionLogViewModelTest
+    /// </summary>
+    [TestClass]
+    public class TransactionLogViewModelTest
+    {
+        [TestMethod]
+        public void ShouldInitializeWithNonEmptyTransactionsCollection()
+        {
+            var vm = new TransactionLogViewModel();
+
+            Assert.IsNotNull(vm.TransactionsCollection);
+
+            CollectionAssert.AllItemsAreInstancesOfType(vm.TransactionsCollection, typeof(Transaction));
+            CollectionAssert.AllItemsAreNotNull(vm.TransactionsCollection);
+            CollectionAssert.AllItemsAreUnique(vm.TransactionsCollection);
+        }
+    }
+}
